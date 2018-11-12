672186dafcdfa438b3b4ff9883889cd39d1a968c
barbosalano <personaltecnologiacg@gmail.com>
2018-11-12 16:27:49 -0300

    Trabalhando com o DELETE AND UPDATE

--- app/src/main/java/com/example/barbosa/bandodedadossqlite/MainActivity.java
+++ app/src/main/java/com/example/barbosa/bandodedadossqlite/MainActivity.java
@@ -19,12 +19,23 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
             //criar tabela
-            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR, idade INT(3))");
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas ( id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INT(3))");
+
+            //para apagar a tabela toda
+            // bancoDados.execSQL("DROP TABLE pessoas");
+
+            //PARA APAGAR UMA LINHA(REGISTRO)
+            bancoDados.execSQL("DELETE FROM pessoas WHERE id = 3");
+
+            //PARA APAGAR todos os registro de uma tabela
+            bancoDados.execSQL("DELETE FROM pessoas ");
+
 
             //Inserir dados
+            //bancoDados.execSQL("INSERT INTO pessoas( nome, idade) VALUES ('Rebeca', 20)");
+          // bancoDados.execSQL("INSERT INTO pessoas( nome, idade) VALUES ('Joao', 23)");
 
-            //bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Roberto', 20)");
-            //bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Juju', 23)");
+            // bancoDados.execSQL("UPDATE pessoas SET nome = 'Luciano Lindo' WHERE nome = 'Luciano'");
 
             //recuperar os dados
             /*String consulta =
@@ -44,8 +55,8 @@ public class MainActivity extends AppCompatActivity {
                     " WHERE idade BETWEEN 20 AND 60";*/
 
             String consulta =
-                    " SELECT nome, idade FROM pessoas " +
-                    " WHERE 1=1 ORDER BY nome ASC LIMIT 2";
+                    "SELECT * FROM pessoas " +
+                            " WHERE 1=1 ";
 
 
 
@@ -53,23 +64,26 @@ public class MainActivity extends AppCompatActivity {
 
             //Indices da tebela
 
+           int indiceId = cursor.getColumnIndex("id");
             int indiceNome = cursor.getColumnIndex("nome");
             int indiceIdade = cursor.getColumnIndex("idade");
 
             cursor.moveToFirst();
 
-            while (cursor != null){
+            while (cursor != null) {
 
+                String id = cursor.getString(indiceId);
                 String nome = cursor.getString(indiceNome);
                 String idade = cursor.getString(indiceIdade);
 
-                Log.i("RESULTADO - nome ", nome + "idade: " + idade);
+
+                Log.i("RESULTADO - id ", id + " / nome: " + nome + " / idade: " + idade);
 
                 cursor.moveToNext();
             }
 
 
-        }catch (Exception e){
+        } catch (Exception e) {
 
             e.printStackTrace();
 
