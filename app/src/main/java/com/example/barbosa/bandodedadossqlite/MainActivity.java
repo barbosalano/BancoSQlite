d8845f9fab35a38ea582ae4b0f0ead332fc69f78
barbosalano <personaltecnologiacg@gmail.com>
2018-11-12 14:17:02 -0300

    Comandos CREATE, INSERT AND SELECT utilisando o SQlite

---
+++ app/src/main/java/com/example/barbosa/bandodedadossqlite/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.barbosa.bandodedadossqlite;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+
+            //Criar banco de dados
+            SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
+
+            //criar tabela
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR, idade INT(3))");
+
+            //Inserir dados
+
+            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Luciano', 30)");
+            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Julia', 60)");
+
+            //recuperar os dados
+            Cursor cursor = bancoDados.rawQuery("SELECT nome, idade FROM pessoas", null);
+
+            //Indices da tebela
+
+            int indiceNome = cursor.getColumnIndex("nome");
+            int indiceIdade = cursor.getColumnIndex("idade");
+
+            cursor.moveToFirst();
+
+            while (cursor != null){
+                Log.i("RESULTADO - nome: ", cursor.getString(indiceNome));
+                Log.i("RESULTADO - idade: ", cursor.getString(indiceIdade));
+                cursor.moveToNext();
+            }
+
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+    }
+}
